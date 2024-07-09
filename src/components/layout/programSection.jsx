b0e8db90d6b23426e73dc8201699fc47fb80f81e
romuloromulo <neoromulo@gmail.com>
2024-07-08 21:23:53 -0300

    ajustando layout de programção

--- src/components/layout/programSection.jsx
+++ src/components/layout/programSection.jsx
@@ -6,16 +6,16 @@ const ProgramSection = ({ id, title, date, program }) => (
     id={id}
     className="relative container bg-background-white/80 w-[80%] py-4 rounded-lg shadow-md mb-12"
   >
-    <div className="flex flex-col items-center lg:px-12 text-zinc-900">
+    <div className="flex flex-col  items-center lg:px-12 text-zinc-900">
       <div className="md:w-auto w-full items-center md:items-start justify-center text-center md:pr-12">
         <h1 className="text-5xl font-bold font-sans whitespace-nowrap">
           {title}
         </h1>
         <p className="text-md sm:text-lg font-semibold">{date}</p>
       </div>
-      <ul className="flex flex-col md:flex-row p-4 rounded-xl text-zinc-800 w-full justify-around items-start">
+      <ul className="flex flex-col md:flex-row p-4 rounded-xl text-zinc-800  items-start">
         {program.map((dia) => (
-          <li key={dia.diaSemana}>
+          <li key={dia.diaSemana} className="w-1/2">
             <div className="items-center w-full gap-2 mb-2">
               <h1
                 className="text-zinc-900 text-2xl md:text-4xl font-bold"
